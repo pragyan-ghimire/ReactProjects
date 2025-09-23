807a3314dfdf3e46510b087ac933bec5cbeaab96
pragyan <ghimirepragyan100@gmail.com>
2025-09-23 20:02:22 +0545

    next: not-found error handle

---
+++ next-app/app/users/[id]/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserNotFoundPage = () => {
+  return (
+    <div>
+        The requested user doesn't exist.
+    </div>
+  )
+}
+
+export default UserNotFoundPage
--- next-app/app/users/[id]/page.tsx
+++ next-app/app/users/[id]/page.tsx
@@ -1,12 +1,14 @@
+import { notFound } from 'next/navigation'
 import { Props } from 'next/script'
 import React from 'react'
 
 interface UserDetailPageProps {
   params: {
-    id: string
+    id: number
   }
 }
 const UserDetailPage = (props: UserDetailPageProps) => {
+  if (props.params.id > 10) notFound();
   return (
     <div>
       <h1>UserDetailPage{props.params.id}</h1>
