e39c6803143b6021e65f2f84cd6480a064ab90d0
pragyan <ghimirepragyan100@gmail.com>
2025-09-23 10:46:15 +0545

    next: layouts

---
+++ next-app/app/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div className='flex bg-slate-200 p-4 gap-4'>
+      <Link href="/">Logo</Link>
+      <Link href="/users">Users</Link>
+    </div>
+  )
+}
+
+export default Navbar
