b3fc374f191217a0ad6b42fb95d1e4b5f98235e0
pragyan <ghimirepragyan100@gmail.com>
2025-09-23 09:32:03 +0545

    next: query parameter

--- next-app/app/entertainment/[[...slug]]/page.tsx
+++ next-app/app/entertainment/[[...slug]]/page.tsx
@@ -3,14 +3,18 @@ import React from 'react'
 interface Props{
     params:{
         slug: string[]
+    };
+    searchParams: {
+      type: string
     }
 }
 
-const Entertainment = async({params}: Props) => {
+const Entertainment = async({params, searchParams}: Props) => {
     const {slug} = await params;
+    const {type} = await searchParams;
   return (
     <div>
-      Entertainment {slug}
+      Entertainment {slug}: {type}
     </div>
   )
 }
