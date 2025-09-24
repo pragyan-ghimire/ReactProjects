95997265bd9bc8f069fc952c57e2d4e0862ca60c
pragyan <ghimirepragyan100@gmail.com>
2025-09-24 10:44:48 +0545

    next: post

---
+++ next-app/app/api/users/[id]/route.tsx
@@ -0,0 +1,12 @@
+import { error } from "console";
+import { NextRequest, NextResponse } from "next/server";
+
+interface Props{
+    params: {id:number}
+}
+
+export function GET(request: NextRequest, {params}:Props) {
+    if(params.id > 10)
+        return NextResponse.json({error: "User not found"}, {status: 404});
+    return NextResponse.json({id: 1, name:"John Doe"});
+}
--- next-app/app/api/users/route.tsx
+++ next-app/app/api/users/route.tsx
@@ -6,5 +6,12 @@ export function GET(request: NextRequest) {
         { id: 1, name:"John Doe"},
         { id: 2, name:"Jane Doe"},
         { id: 3, name:"Jim Doe"},
-    ]);
+    ]); 
+}
+
+export async function POST(request: NextRequest){
+    const body = await request.json();
+    if(!body.name)
+        return NextResponse.json({error:"Name is required"}, {status:400});
+    return NextResponse.json({id: 1, name: body.name});
 }
