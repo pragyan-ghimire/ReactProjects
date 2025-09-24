7eda34f5bb138d0ed1c99d81ccae0bdbe83e0d0f
pragyan <ghimirepragyan100@gmail.com>
2025-09-24 14:34:45 +0545

    put and delete

--- next-app/app/api/users/[id]/route.tsx
+++ next-app/app/api/users/[id]/route.tsx
@@ -10,3 +10,27 @@ export function GET(request: NextRequest, {params}:Props) {
         return NextResponse.json({error: "User not found"}, {status: 404});
     return NextResponse.json({id: 1, name:"John Doe"});
 }
+
+export async function PUT(request: NextRequest, {params}:Props){
+    // validate the request body
+    // if invalid, return 400
+    // fetch the user with the given id
+    // if doesn't exist, return 404
+    // update user
+    // return updated user
+    const body = await request.json()
+    if(!body.name)
+        return NextResponse.json({error: "name is required"}, {status: 400})
+    if(params.id > 10)
+        return NextResponse.json({error:"User not found"}, {status: 404})
+    return NextResponse.json({id:1, name: body.name})
+}
+
+export async function DELETE(request: NextRequest, {params}:Props){
+    // fetch the user from db
+    // if doesn't exist, return 404
+    // delete user and return 200
+if(params.id > 10)
+        return NextResponse.json({error:"User not found"}, {status: 404})
+    return NextResponse.json({})
+}
