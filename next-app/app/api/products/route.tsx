dc36332b19bbf74ad9056c281bd3161abb4046f7
pragyan <ghimirepragyan100@gmail.com>
2025-09-25 18:21:40 +0545

    excercise: product db api

--- next-app/app/api/products/route.tsx
+++ next-app/app/api/products/route.tsx
@@ -1,20 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ProductSchema } from "./schema";
-import { parse } from "path";
+import {prisma} from "@/prisma/client"
 
-export function GET(request: NextRequest){
-    return NextResponse.json([
-        {
-            id: 1,
-            name: 'Product 1',
-            price: 100,
-        },
-        {
-            id: 2,
-            name: 'Product 2',
-            price: 200,
-        }   
-    ])
+export async function GET(request: NextRequest){
+    const product = await prisma.product.findMany();
+    return NextResponse.json(product);
+ 
+    // return NextResponse.json([
+    //     {
+    //         id: 1,
+    //         name: 'Product 1',
+    //         price: 100,
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Product 2',
+    //         price: 200,
+    //     }   
+    // ])
 }
 
 export async function POST(request: NextRequest){
@@ -24,7 +27,16 @@ export async function POST(request: NextRequest){
     if(!parseProduct.success){
         return NextResponse.json(parseProduct.error.issues, {status: 400});
     }
+
+    const product = await prisma.product.create({
+        data: {
+            name: parseProduct.data.name,
+            price: parseProduct.data.price,
+            description: body.description || null,
+        }
+    })
+    return NextResponse.json(product, {status: 201});
     // return NextResponse.json({id: 1, data: parseProduct.data}, {status: 201});
-    return NextResponse.json({id: 1, ...parseProduct.data}, {status: 201});
+    // return NextResponse.json({id: 1, ...parseProduct.data}, {status: 201});
 
 }
--- next-app/app/api/products/schema.ts
+++ next-app/app/api/products/schema.ts
@@ -4,5 +4,6 @@ export const ProductSchema = z.object(
     {
         name: z.string().min(3, 'Name should be at least 3 characters long'),
         price: z.number().min(0, 'Price should be a positive number'),
+        descritption: z.string().optional(),
     }
 )
