9f6a416bbca559b8a105f9c085230f8a3534984e
pragyan <ghimirepragyan100@gmail.com>
2025-09-26 19:36:51 +0545

    access session on client

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar";
-
+import AuthProvider from "./auth/Provider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,8 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar></Navbar>
-        <main>{children}</main>
+        <AuthProvider> 
+          <Navbar></Navbar>
+          <main>{children}</main>
+        </AuthProvider>
       </body>
     </html>
   );
