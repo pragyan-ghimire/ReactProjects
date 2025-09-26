a457a82d0f18b2bd48666f65199ba5356bca97f5
pragyan <ghimirepragyan100@gmail.com>
2025-09-26 20:06:00 +0545

    next: access session server and signout

--- next-app/app/Navbar.tsx
+++ next-app/app/Navbar.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+"use client"
+import React, { use } from 'react'
 import Link from 'next/link'
+import { useSession } from 'next-auth/react'
 
 const Navbar = () => {
+  const {status, data: session} = useSession();
   return (
     <div className='flex bg-slate-200 p-4 gap-4'>
       <Link href="/">Logo</Link>
       <Link href="/users">Users</Link>
-      <Link href="/api/auth/signin">Sign in</Link>
+      {status === "loading" && <p>Loading...</p>}
+      {status === "authenticated" && 
+      <div>
+        Welcome, {session.user?.name}
+        <Link href="/api/auth/signout" className='ml-4'>Sign out</Link>
+      </div>
+      }
+      {status === "unauthenticated" && <Link href="/api/auth/signin">Sign in</Link>}
+      
     </div>
   )
 }
